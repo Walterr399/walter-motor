0b892a6adaf013addca957643da946947c2cdc2a
Walterr399 <walterwhiterp47@gmail.com>
2025-07-05 14:56:29 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,24 @@
-fx_version "cerulean"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-author "Walterr399"
-description "A simple resource mostly used by easy weapons servers."
-version "1.0.0"
+author 'Walterr399'
+description 'A simple resource mostly used by Easy Weapons servers.'
+version '1.0.0'
 
-client_scripts { "client/*.lua" }
-server_scripts { "server/*.lua", "config.lua" }
-shared_scripts { "@vx_lib/init.lua" }
+shared_scripts {
+    '@vx_lib/init.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua',
+    'config.lua'
+}
 
 dependencies {
-    "ox_lib",
-    "vx_lib",
+    'vx_lib'
 }
