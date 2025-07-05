587f58700e44f6385f3b0a756983c334f6460b2f
Walterr399 <walterwhiterp47@gmail.com>
2025-07-05 14:55:29 +0200

    Depedency ox lib removed.

--- server/main.lua
+++ server/main.lua
@@ -11,7 +11,7 @@ vx.addCommand('motor', {
 
     if cooldown[src] and os - cooldown[src] < 300 then
         local remaining = 300 - (os - cooldown[src])
-        TriggerClientEvent("ox_lib:notify", src, {
+        vx.notify(src, {
             description = "You have to wait " .. remaining .. " seconds before you can spawn a motor again.",
             position = "left-center",
             type = "error"
